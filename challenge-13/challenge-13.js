f8f5468e9b0efaca8d0379d2896a99e33cc5ca88
Lucas Fernando <lucasfernandosantos2001@gmail.com>
2021-11-02 22:58:48 -0300

    quase finalizando a lista

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -1,12 +1,15 @@
-/*
+(()=>{
+  /*
 Envolva todo o código desse desafio em uma IIFE.
 */
-
+const myArray = ['el1','el2','el3'];
 /*
 Crie um array e mostre no console a representação em String desse array,
 usando o método visto na aula 13.
+
+
 */
-console.log( 'O array em formato de string é:' );
+console.log( 'O array em formato de string é: ' + myArray.join(', '));
 // ?
 
 /*
@@ -15,23 +18,35 @@ Cada array deve conter os estados dessa região.
 */
 // ?
 
+const sul = ['Paraná', 'Santa Catarina', 'Rio Grande do Sul']; const sudeste = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais'];
+
 /*
 Crie uma variável chamada `brasil`, que irá receber as duas regiões
 concatenadas. Mostre o `brasil` no console.
+
 */
-console.log( '\nAlguns Estados do Brasil:' );
+
+const brasil = sul.concat(...sudeste);
+console.log( '\nAlguns Estados do Brasil:' + brasil);
+
+console.log(brasil)
 // ?
 
 /*
 Adicione 3 novos estados da região Norte no início do array e mostre no console.
 */
-console.log( '\nMais estados adicionados:' );
+
+const norte = ['Amazonas', 'Pará', 'Amapá']
+brasil.unshift(...norte)
+console.log( '\nMais estados adicionados:' + brasil.join(', ') );
 // ?
 
 /*
 Remova o primeiro estado do array `brasil` e mostre-o no console.
 */
-console.log( '\nEstado removido:' );
+
+
+console.log( '\nEstado removido: ' + brasil.shift() );
 // ?
 
 /*
@@ -40,16 +55,27 @@ pegando do array `brasil`. Não remova esses itens de `brasil`.
 */
 // ?
 
+const newSul = [];
+
+brasil.forEach((estado)=>{
+  sul.forEach((estSul)=>{
+    if(estSul === estado){
+       newSul.push(estado);
+    }
+  })
+});
+
+
 /*
 Mostre no console os estados que estão em `newSul`.
 */
-console.log( '\nEstados do Sul do Brasil:' );
+console.log( '\nEstados do Sul do Brasil:' + newSul.join(', '));
 // ?
 
 /*
 Mostre no console todos os estados que estão em `brasil`.
 */
-console.log( '\nAlguns Estados do Brasil:' );
+console.log( '\nAlguns Estados do Brasil:' + brasil.join(', '));
 // ?
 
 /*
@@ -57,11 +83,15 @@ Crie um novo array chamado `nordeste`, que tenha os estados do nordeste.
 */
 // ?
 
+const nordeste = ['Bahia', 'Ceará', 'Piauí'];
+
 /*
 Mostre no console os estados do nordeste.
 */
-console.log( '\nEstados do Nordeste:' );
+console.log( '\nEstados do Nordeste:' + nordeste.join(', '));
 // ?
+
+
 
 /*
 Remova de `brasil` os estados do `sudeste`, colocando-os em uma variável
@@ -69,22 +99,32 @@ chamada `newSudeste`.
 */
 // ?
 
+
+const newSudeste = []
+sudeste.forEach((estSudeste)=>{
+  newSudeste.push(brasil.splice(brasil.indexOf(estSudeste), 1));
+})
+
+console.log('--Esse é o brasil: ' + brasil.join(', '))
+
 /*
 Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
 ficar no mesmo nível que os estados já existentes, não em um array separado.
 */
 // ?
 
+brasil.push(...nordeste);
+
 /*
 Mostre no console os estados em `newSudeste`.
 */
-console.log( '\nEstados em newSudeste:' );
+console.log( '\nEstados em newSudeste: ' + newSudeste);
 // ?
 
 /*
 Mostre no console os estados do `brasil`.
 */
-console.log( '\nAlguns estados do Brasil:' );
+console.log( '\nAlguns estados do Brasil:' + brasil.join(', '));
 // ?
 
 /*
@@ -96,10 +136,22 @@ propriedades:
 */
 // ?
 
+const newBrasil = [];
+function estado(id, estado){
+  this.estado = estado;
+  this.id = id;
+}
+
+brasil.forEach((estBrasil, index)=>{
+  const obj = new estado(index,estBrasil);
+  newBrasil.push(obj);
+})
+
+
 /*
 Mostre o array `newBrasil` no console
 */
-console.log( '\nnewBrasil:' );
+console.log(newBrasil);
 // ?
 
 /*
@@ -148,3 +200,4 @@ Mostre o array filtrado acima no console.
 */
 console.log( '\nEstados com ID par:' );
 // ?
+})();
